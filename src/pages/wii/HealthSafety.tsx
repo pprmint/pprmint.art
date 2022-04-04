c60f0ca681dab36b979f7a94ba5167fa004ae877
pprmint <29925944+pprmint@users.noreply.github.com>
2022-04-04 21:49:48 +0200

    Add easter egg that's less of an easter egg now that this commit spoiled it.

---
+++ src/pages/wii/HealthSafety.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import { Container, Grid } from "@mui/material";
+
+import Text from "./assets/text.svg";
+import ButtonYes from "./assets/yes_button.svg";
+import ButtonYesHover from "./assets/yes_button_hover.svg";
+import ButtonNoDisabled from "./assets/no_button_disabled.svg";
+import PopupSound from "./assets/Sample_0021.wav";
+import HoverSound from "./assets/Sample_0004.wav";
+import ClickSound from "./assets/Sample_0005.wav";
+import ErrorSound from "./assets/Sample_0011.wav";
+
+import "./assets/popupStyles.scss";
+
+function HealthSafety() {
+	let hover = new Audio(HoverSound);
+	let click = new Audio(ClickSound);
+	let error = new Audio(ErrorSound);
+	return (
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Mintendo - Health and Safety Precautions</title>
+				<meta name="description" content="Not yet." />
+			</Helmet>
+			<Container className="title" maxWidth="md">
+				<div className="WiiPopup">
+					<audio autoPlay>
+						<source src={PopupSound} />
+					</audio>
+					<img src={Text} style={{ margin: "20% 15% 17%" }} />
+					<Grid container>
+						<Grid item xs={6} id="yesButton">
+							<Link to="/">
+								<img src={ButtonYes} onMouseOver={() => hover.play()} />
+								<img src={ButtonYesHover} onClick={() => click.play()} />
+							</Link>
+						</Grid>
+						<Grid item xs={6}>
+							<img src={ButtonNoDisabled} onClick={() => error.play()} />
+						</Grid>
+					</Grid>
+				</div>
+			</Container>
+		</>
+	);
+}
+
+export default HealthSafety;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -8,5 +8,9 @@ declare module "*.webm" {
   const src: string;
   export default src;
 }
+declare module "*.wav" {
+  const src: string;
+  export default src;
+}
 
 declare module 'react-helmet';
